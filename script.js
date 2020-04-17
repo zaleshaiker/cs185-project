89649df7b315f6a6c2bb772d7521c02a4aa6b305
Zain Aleshaiker <zalind@gmail.com>
2020-04-16 23:48:21 -0700

    Added back to top button and popup images on click

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function onload(page) {
+function onLoad(page) {
 	$('#header').load('templates/header.html', null, function() {
-		$('#menu-' + page).css('color', 'gray');
+		if (page !== '') {
+			$('#menu-' + page).css('color', 'gray');
+		}
 	});
 	$('#footer').load('templates/footer.html');
+	
+	if (page === 'images') {
+		window.onscroll = function() {
+			if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+				$('.back-to-top').css('display', 'block');
+			} else {
+				$('.back-to-top').css('display', 'none');
+			}
+		};
 
+	}
 }
+
+function backToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+} 
